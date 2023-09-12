1186e874869530478f6ffc6d4f432a3e77e0f570
Brage Imset <Braimset@gmail.com>
2023-09-12 14:52:05 +0200

    Sample with ADC in main

--- src/node1/main.c
+++ src/node1/main.c
@@ -10,8 +10,9 @@
 #include <stdbool.h>
 #define F_CPU 4915200 // Hz
 #include <util/delay.h> // Uses F_CPU
-
+#include "adc_driver.h"
 #include "uart_driver.h"
+#include "hardware_tests.h"
 
 #define BAUD_RATE 9600
 
@@ -24,12 +25,17 @@ int main(void)
 	stdout = fdevopen(uart_putchar, NULL);
 	uart_init(F_CPU, BAUD_RATE);
 	sei();
-	
+	adc_init();
+
 	printf("Starting up...\n");
+	
 	unsigned char received[100];
 	size_t num_received = 0;
 	while (true) {
 		num_received = uart_receive(received, 100);
+		if(num_received > 0) {
+			printf("Reading from ADC:\nChannel 0: %u\nChannel 1: %u\nChannel 2: %u\nChannel 3: %u\n\n", adc_read(0), adc_read(1), adc_read(2), adc_read(3));
+		}
 		uart_transmit(received, num_received);
 		_delay_ms(100);
 	}
